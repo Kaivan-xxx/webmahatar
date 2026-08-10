285beeb8e74ae21a41c4fb08283d2f2dd11007a8
DESKTOP-IV7L21G\Barayudha Arkano <yudoarkano2@gmail.com>
2026-08-10 18:44:40 +0700

    php

---
+++ config/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_website_mahatar";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+
+?>
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
+<?php
+include "config/koneksi.php";
 
+echo "Koneksi database berhasil!";
+?>
