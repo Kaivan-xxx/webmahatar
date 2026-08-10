bb66471404decaf9affa4a9b82a397c7cc034790
DESKTOP-IV7L21G\Barayudha Arkano <yudoarkano2@gmail.com>
2026-08-10 19:11:39 +0700

    php

--- config/koneksi.php
+++ config/koneksi.php
@@ -11,4 +11,6 @@ if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
 
+echo "Koneksi database berhasil!";
+
 ?>
--- login.php
+++ login.php
@@ -1,1 +1,58 @@
+<?php
 
+include "config/koneksi.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE username = '$username'";
+
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+
+        echo "Username ditemukan!";
+
+    } else {
+
+        echo "Username tidak ditemukan!";
+
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Admin Mahatar</title>
+</head>
+
+<body>
+
+    <h1>Login Admin Mahatar</h1>
+
+    <form method="POST">
+
+        <label>Username</label>
+        <br>
+        <input type="text" name="username">
+
+        <br><br>
+
+        <label>Password</label>
+        <br>
+        <input type="password" name="password">
+
+        <br><br>
+
+        <button type="submit">Login</button>
+
+    </form>
+
+</body>
+</html>
