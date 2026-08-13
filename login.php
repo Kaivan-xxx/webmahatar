614061a7bc38e5d3653c24ef86e1ab9bae57fee2
DESKTOP-IV7L21G\Barayudha Arkano <yudoarkano2@gmail.com>
2026-08-13 10:48:43 +0700

    keamanan login

--- login.php
+++ login.php
@@ -9,9 +9,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $query = "SELECT * FROM users WHERE username = '$username'";
+      $stmt = mysqli_prepare(
+        $conn,
+        "SELECT * FROM users WHERE username = ?"
+    );
 
-    $result = mysqli_query($conn, $query);
+    mysqli_stmt_bind_param($stmt, "s", $username);
+
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
 
     if (mysqli_num_rows($result) > 0) {
 
