2998a29daef03f99ee8c62c91b19658cc579f090
DESKTOP-IV7L21G\Barayudha Arkano <yudoarkano2@gmail.com>
2026-08-18 08:41:51 +0700

    Tambah berita

---
+++ admin/berita/index.php
@@ -0,0 +1,77 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['id_user'])) {
+    header("Location: ../../login.php");
+    exit;
+}
+
+include '../../config/koneksi.php';
+
+include "../../config/koneksi.php";
+
+$query = "SELECT * FROM berita ORDER BY tanggal DESC";
+
+$result = mysqli_query($conn, $query);
+
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Kelola Berita</title>
+
+</head>
+
+<body>
+
+    <h1>Kelola Berita</h1>
+
+    <p>
+        Selamat datang, ke dalam dashboard Bertia
+    </p>
+
+   <a href="tambah.php">
+        Tambah Berita
+    </a>
+
+    <br><br>
+
+    <a href="../dashboard.php">
+        Kembali ke Dashboard
+    </a>
+
+    <hr>
+
+    <h2>Daftar Berita</h2>
+
+    <?php
+
+    while ($berita = mysqli_fetch_assoc($result)) {
+
+    ?>
+
+        <h3>
+            <?php echo $berita['judul']; ?>
+        </h3>
+
+        <p>
+            <?php echo $berita['isi']; ?>
+        </p>
+
+        <hr>
+
+    <?php
+
+    }
+
+    ?>
+</body>
+
+</html>
