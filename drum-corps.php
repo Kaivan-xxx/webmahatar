1ca9e292a07f4fbd2263d7f0dd21f42d19a7bf74
Gilang <gdwikananda@gmail.com>
2026-09-21 21:32:53 +0700

    ganti foto

--- drum-corps.php
+++ drum-corps.php
@@ -216,7 +216,7 @@ $result = mysqli_stmt_get_result($stmt);
           style="display: flex; gap: 20px; align-items: center; flex-wrap: wrap"
         >
           <img
-            src="assets/image/profile.jpg"
+            src="assets/image/DSC08797.JPG"
             alt="Drum Corps AMNI"
             style="
               width: 100%;
--- pedang-pora.php
+++ pedang-pora.php
@@ -214,7 +214,7 @@ $result = mysqli_stmt_get_result($stmt);
           style="display: flex; gap: 20px; align-items: center; flex-wrap: wrap"
         >
           <img
-            src="assets/image/profile.jpg"
+            src="assets/image/DSC02271.jpg.jpeg"
             alt="Pedang Pora AMNI"
             style="
               width: 100%;
